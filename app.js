4d7c5fc30d8e7fcbe98095aa46b70bc9bcefd2b2
pgm <affirming_km@naver.com>
2021-01-15 14:04:47 +0900

    express final

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
+const nunjucks = require('nunjucks');
 
 dotenv.config();
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
+
 const app = express();
 app.set('port', process.env.PORT || 3000);
+//app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -25,15 +36,18 @@ app.use(session({
     name: 'session-cookie',
 }));
 
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
 const multer = require('multer');
 const fs = require('fs');
 
-try{
-    fs.readFileSync('uploads');
-}catch(err){
-    console.error('uploads 폴더가 없어 uploads 폴터를 생성합니다.');
-    fs.mkdirSync('uploads');
-}
 const upload = multer({
     storage: multer.diskStorage({
         destination(req, file, done){
@@ -70,8 +84,10 @@ app.get('/', (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send(err.message);
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.listen(app.get('port'), () => {
